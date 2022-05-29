fb6077523a6db39b86a6a39b0f0e5b3873fa528a
reissouza <reis.souza@outlook.com.br>
2022-05-28 22:12:26 -0300

    feat: :sparkles: mask for code and mask cpfOurCnpj

--- src/app.ts
+++ src/app.ts
@@ -7,15 +7,6 @@ const cnpjMask = (value: string) => {
     .replace(/(\d{4})(\d{1,2})/, "$1-$2")
     .replace(/(-\d{2})\d+?$/, "$1");
 };
-const telMask = (value: string) => {
-  return value
-    .replace(/\D/g, "")
-    .replace(/(\d{0})(\d)/, "$1($2")
-    .replace(/(\d{2})(\d)/, "$1) $2")
-    .replace(/(\d{1})(\d+?$)/, "$1 $2")
-    .replace(/(\d{4})(\d{1,2})/, "$1-$2")
-    .replace(/(-\d{4})\d+?$/, "$1");
-};
 
 export const cpfMask = (value: string) => {
   return value
@@ -26,9 +17,32 @@ export const cpfMask = (value: string) => {
     .replace(/(-\d{2})\d+?$/, "$1");
 };
 
+export const cpfOurCnpj = (value: string) => {
+  if (value.length <= 14) {
+    return cpfMask(value);
+  }
+  return cnpjMask(value);
+};
+
+export const codeMask = (value: string) => {
+  return value
+    .replace(/\D/g, "")
+    .replace(/(\d{5})(\d)/, "$1-$2")
+    .replace(/(-\d{3})\d+?$/, "$1");
+};
+
+const telMask = (value: string) => {
+  return value
+    .replace(/\D/g, "")
+    .replace(/(\d{0})(\d)/, "$1($2")
+    .replace(/(\d{2})(\d)/, "$1) $2")
+    .replace(/(\d{1})(\d+?$)/, "$1 $2")
+    .replace(/(\d{4})(\d{1,2})/, "$1-$2")
+    .replace(/(-\d{4})\d+?$/, "$1");
+};
 
 type maskInputProps = {
-  type: "cnpj" | "cpf" | "tel";
+  type: "cnpj" | "cpf" | "tel" | "code" | "cpfOurCnpj";
   value: string;
 };
 const maskInput = ({ type, value }: maskInputProps) => {
@@ -36,11 +50,19 @@ const maskInput = ({ type, value }: maskInputProps) => {
     case "cnpj": {
       return cnpjMask(value);
     }
-    case 'cpf':{
-        return cpfMask(value)
+    case "cpf": {
+      return cpfMask(value);
     }
-    case 'tel': {
-        return telMask(value)
+    case "tel": {
+      return telMask(value);
+    }
+    case "code": {
+      return codeMask(value);
+    }
+    case "cpfOurCnpj": {
+      return cpfOurCnpj(value);
     }
   }
 };
+
+
